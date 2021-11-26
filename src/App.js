c126dacdf73d0efe04e13ec1a1c40321fae49afc
Rob Kreisel <kreisel@mac.com>
2021-11-25 20:41:36 -0500

    Progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App2() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div className="m-3 p-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
+      <div className="text-gray-400">History</div>
+      <div>
+      <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
+        <div>
+          <div className="text-sm font-medium text-black hidden sm:block">1430</div>
+          <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
+          <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
+          <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
+          <p className="font-medium text-black text-center hidden sm:block">Moses</p>
+        </div>
+      </div>
+      </div>
     </div>
+    <div className="m-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
+      <div className="text-gray-400">Writings</div>
+    </div>
+    <div className="m-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
+      <div className="text-gray-400">Prophetic</div>
+    </div>
+  </div>
   );
 }
 
-export default App;
+export default App2;
