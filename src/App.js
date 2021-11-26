9c4bb81c807af4c8f7799ff396f371d266d94f66
Rob Kreisel <kreisel@mac.com>
2021-11-25 22:09:36 -0500

    Extracted Book and added data

--- src/App.js
+++ src/App.js
@@ -1,120 +1,17 @@
 import React from 'react';
 import './App.css';
+import Book from './components/book';
+import books from './data/books';
 
-function App2() {
+function App() {
   return (
   <div>
     <div className="m-3 p-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
       <div className="text-gray-400">History</div>
       <div className="flex flex-wrap gap-1">
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div> 
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div> 
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>
-        <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">1430</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">BC</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">Ge</div>
-            <p class="text-gray-500 text-center hidden sm:block">GENESIS</p>
-            <p className="font-medium text-black text-center hidden sm:block">Moses</p>
-          </div>
-        </div>  
+        {books.map((book) => (
+          <Book book={book} key={books.symbol} />
+        ))}
       </div>
     </div>
     <div className="m-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
@@ -127,4 +24,4 @@ function App2() {
   );
 }
 
-export default App2;
+export default App;
---
+++ src/data/books.js
@@ -0,0 +1,121 @@
+export default [
+{
+    name: "Genesis",
+    symbol: "Ge",
+    year: "1430",
+    era: "BC",
+    author: "Moses"
+},
+{
+    name: "Exodus",
+    symbol: "Ex",
+    year: "1400",
+    era: "BC",
+    author: "Moses"
+},
+{
+    name: "Leviticus",
+    symbol: "Lv",
+    year: "1445",
+    era: "BC",
+    author: "Moses"
+},
+{
+    name: "Numbers",
+    symbol: "Nu",
+    year: "1400",
+    era: "BC",
+    author: "Moses"
+},
+{
+    name: "Deuteronomy",
+    symbol: "Dt",
+    year: "1400",
+    era: "BC",
+    author: "Moses"
+},
+{
+    name: "Joshua",
+    symbol: "Js",
+    year: "1375",
+    era: "BC",
+    author: "Joshua"
+},
+{
+    name: "Judges",
+    symbol: "Ju",
+    year: "1050",
+    era: "BC",
+    author: "Samuel"
+},
+{
+    name: "Ruth",
+    symbol: "Ru",
+    year: "1050-1000",
+    era: "BC",
+    author: "Samuel"
+},
+{
+    name: "1 Samuel",
+    symbol: "1Sa",
+    year: "900",
+    era: "BC",
+    author: "Samuel"
+},
+{
+    name: "1 Samuel",
+    symbol: "2Sa",
+    year: "900",
+    era: "BC",
+    author: "Samuel"
+},
+{
+    name: "1 Kings",
+    symbol: "1Ki",
+    year: "550",
+    era: "BC",
+    author: "Unknown"
+},
+{
+    name: "2 Kings",
+    symbol: "2Ki",
+    year: "550",
+    era: "BC",
+    author: "Unknown"
+},
+{
+    name: "1 Chronicles",
+    symbol: "1Ch",
+    year: "450",
+    era: "BC",
+    author: "Unknown"
+},
+{
+    name: "2 Chronicles",
+    symbol: "2Ch",
+    year: "340",
+    era: "BC",
+    author: "Unknown"
+},
+{
+    name: "Ezra",
+    symbol: "Er",
+    year: "450",
+    era: "BC",
+    author: "Ezra"
+},
+{
+    name: "Nehemiah",
+    symbol: "Ne",
+    year: "425-400",
+    era: "BC",
+    author: "Nehemiah"
+},
+{
+    name: "Esther",
+    symbol: "Es",
+    year: "450",
+    era: "BC",
+    author: "Unknown"
+}
+];
