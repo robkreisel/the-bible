65c09781d15c224817d9c31f5223017bdbbf9bf9
Rob Kreisel <kreisel@mac.com>
2021-11-25 22:36:21 -0500

    Added historical and writings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-import Book from './components/book';
-import books from './data/books';
+import Book from './components/Book';
+import history from './data/otHistorical';
+import writings from './data/otWritings';
 
 function App() {
   return (
@@ -9,15 +10,20 @@ function App() {
     <div className="m-3 p-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
       <div className="text-gray-400">History</div>
       <div className="flex flex-wrap gap-1">
-        {books.map((book) => (
-          <Book book={book} key={books.symbol} />
+        {history.map((book) => (
+          <Book book={book} key={history.symbol} />
         ))}
       </div>
     </div>
-    <div className="m-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
+    <div className="m-3 p-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
       <div className="text-gray-400">Writings</div>
+      <div className="flex flex-wrap gap-1">
+        {writings.map((book) => (
+          <Book book={book} key={writings.symbol} />
+        ))}
+      </div>
     </div>
-    <div className="m-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
+    <div className="m-3 p-3 sm:m-10 bg-gray-100 shadow-md overflow-hidden">
       <div className="text-gray-400">Prophetic</div>
     </div>
   </div>
---
+++ src/data/otWritings.js
@@ -0,0 +1,37 @@
+export default [
+{
+    name: "Job",
+    symbol: "Jb",
+    year: "1900",
+    era: "BC",
+    author: "Unknown"
+},
+{
+    name: "Psalms",
+    symbol: "Ps",
+    year: "1400-450",
+    era: "BC",
+    author: "Various"
+},
+{
+    name: "Proverbs",
+    symbol: "Pr",
+    year: "970-675",
+    era: "BC",
+    author: "Solomon"
+},
+{
+    name: "Ecclesiastes",
+    symbol: "Ec",
+    year: "940-931",
+    era: "BC",
+    author: "Solomon"
+},
+{
+    name: "Song of Songs",
+    symbol: "Ss",
+    year: "970-950",
+    era: "BC",
+    author: "Solomon"
+}
+];
