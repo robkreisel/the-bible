d7eedf28fbfc0d226b4789594d1d0010e81688c5
Rob Kreisel <kreisel@mac.com>
2021-11-26 01:38:50 -0500

    Extracted book details

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -1,15 +1,10 @@
 import React from "react";
+import BookDetails from "./BookDetails";
 
 export default function Book({ book }) {
     return (
         <div class="p-2 sm:p-6 bg-blue-300 hover:bg-blue-400 rounded-md shadow-md w-10 sm:w-40">
-          <div>
-            <div className="text-sm font-medium text-black hidden sm:block">{book.year}</div>
-            <div className="text-sm font-light text-gray-500 hidden sm:block">{book.era}</div>
-            <div class="text-sm sm:text-6xl font-bold text-black text-center">{book.symbol}</div>
-            <p class="text-gray-500 text-center hidden sm:block">{book.name}</p>
-            <p className="font-medium text-black text-center hidden sm:block">{book.author}</p>
-          </div>
+          <BookDetails book={book} key={book.symbol} />
         </div>
     );
 }
