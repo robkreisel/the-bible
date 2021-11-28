a38d4866b3ef6e0abcab2ca78e20d94869a44293
Rob Kreisel <kreisel@mac.com>
2021-11-27 20:53:48 -0500

    Style changes

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -5,8 +5,8 @@ export default function BookDetails({book}) {
         <div>
             <div className="text-gray-500 text-center hidden md:block">{book.name}</div>
             <div className="text-sm md:text-5xl font-bold text-black text-center md:mb-2">{book.symbol}</div>
-            <div className="text-xs font-medium text-gray-500 text-center hidden md:block">{book.author}</div>
-            <div className="text-xs font-medium text-gray-500 text-center hidden md:block">{book.year}</div>
+            <div className="text-xs font-medium text-gray-500 text-center hidden lg:block">{book.author}</div>
+            <div className="text-xs font-medium text-gray-500 text-center hidden lg:block">{book.year}</div>
         </div>
     );
 }
