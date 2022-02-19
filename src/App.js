d06a56977a54dbfdc18456a3f0550820f764fc59
Rob Kreisel <rob@deisgn1010.com>
2022-02-19 12:43:51 -0600

    Added Nav buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
-import OTDetails from './components/OTDetails';
-import NTDetails from './components/NTDetails';
-import OTChronological from './components/OTChronological';
+import React from "react";
+import "./App.css";
+import OTDetails from "./components/OTDetails";
+import NTDetails from "./components/NTDetails";
+import OTChronological from "./components/OTChronological";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div>
+      <Nav />
       <div className="hidden">
         <OTDetails />
       </div>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div>
+      <button className="rounded-none bg-gray-200 p-3 m-2">HS-Cat</button>
+      <button className="rounded-none bg-gray-200 p-3 m-2">NC-Cat</button>
+      <button className="rounded-none bg-gray-200 p-3 m-2">HS-Chron</button>
+      <button className="rounded-none bg-gray-200 p-3 m-2">NC-Chron</button>
+    </div>
+  );
+}
