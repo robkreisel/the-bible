9e288a36a6a24b03b1e5a20a1eb4dea253334ca7
Rob Kreisel <rob@deisgn1010.com>
2022-02-19 22:10:59 -0600

    Added BookSet

--- src/components/OTChronological.jsx
+++ src/components/OTChronological.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Book from "./Book";
-import data from "../data/otBookInformation";
+import BookSet from "./BookSet";
 
 export default function OTChronological() {
   const kingdomR2Symbols = ["2Sa", "1Ch", "Ps"];
@@ -26,19 +25,11 @@ export default function OTChronological() {
     <div>
       <div className="m-3 p-3 md:m-10 bg-gray-100 shadow-md overflow-hidden">
         <div className="mb-3 text-gray-400">Creation</div>
-        <div className="flex flex-wrap gap-1">
-          {data
-            .filter(function (book) {
-              return book.era.includes("creation");
-            })
-            .map((book) => (
-              <Book book={book} key={book.symbol} />
-            ))}
-        </div>
+        <BookSet symbols={["Ge"]} />
       </div>
       <div className="m-3 p-3 md:m-10 bg-gray-100 shadow-md overflow-hidden">
         <div className="mb-3 text-gray-400">Patriarch</div>
-        <div className="flex">
+        {/* <div className="flex">
           <div className="grid grid-cols-2 gap-1">
             {data
               .filter(function (book) {
@@ -48,11 +39,11 @@ export default function OTChronological() {
                 <Book book={book} key={book.symbol} />
               ))}
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="m-3 p-3 md:m-10 bg-gray-100 shadow-md overflow-hidden">
         <div className="mb-3 text-gray-400">Exodus</div>
-        <div className="flex">
+        {/* <div className="flex">
           <div className="grid grid-rows-2 grid-cols-2 gap-1">
             {data
               .filter(function (book) {
@@ -62,11 +53,11 @@ export default function OTChronological() {
                 <Book book={book} key={book.symbol} />
               ))}
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="m-3 p-3 md:m-10 bg-gray-100 shadow-md overflow-hidden">
         <div className="mb-3 text-gray-400">Conquest</div>
-        <div className="flex flex-wrap gap-1">
+        {/* <div className="flex flex-wrap gap-1">
           {data
             .filter(function (book) {
               return book.era.includes("conquest");
@@ -74,11 +65,11 @@ export default function OTChronological() {
             .map((book) => (
               <Book book={book} key={book.symbol} />
             ))}
-        </div>
+        </div> */}
       </div>
       <div className="m-3 p-3 md:m-10 bg-gray-100 shadow-md overflow-hidden">
         <div className="mb-3 text-gray-400">Judges</div>
-        <div className="flex">
+        {/* <div className="flex">
           <div className="grid grid-cols-2 gap-1">
             {data
               .filter(function (book) {
@@ -88,11 +79,12 @@ export default function OTChronological() {
                 <Book book={book} key={book.symbol} />
               ))}
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="m-3 p-3 md:m-10 bg-gray-100 shadow-md overflow-hidden">
         <div className="mb-3 text-gray-400">Kingdom</div>
-        <div className="pb-1">
+        <BookSet symbols={kingdomR3Symbols} />
+        {/* <div className="pb-1">
           <Book
             book={data.find(function (book) {
               return book.symbol === "1Sa";
@@ -138,11 +130,11 @@ export default function OTChronological() {
               />
             );
           })}
-        </div>
+        </div> */}
       </div>
       <div className="m-3 p-3 md:m-10 bg-gray-100 shadow-md overflow-hidden">
         <div className="mb-3 text-gray-400">Exile</div>
-        <div className="flex flex-wrap gap-1">
+        {/* <div className="flex flex-wrap gap-1">
           {exileSymbols.map(function (symbol) {
             return (
               <Book
@@ -153,11 +145,11 @@ export default function OTChronological() {
               />
             );
           })}
-        </div>
+        </div> */}
       </div>
       <div className="m-3 p-3 md:m-10 bg-gray-100 shadow-md overflow-hidden">
         <div className="mb-3 text-gray-400">Return</div>
-        <div className="flex pb-1">
+        {/* <div className="flex pb-1">
           <div className="grid grid-cols-5 gap-1">
             {returnR1Symbols.map(function (symbol) {
               return (
@@ -184,7 +176,7 @@ export default function OTChronological() {
               );
             })}
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
