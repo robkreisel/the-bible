10d6509e130e146191d6fa55b9c8291b487153fd
Rob Kreisel <rob@deisgn1010.com>
2022-02-19 19:55:33 -0600

    UI improvements

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,22 +1,41 @@
 import React from "react";
 
-
 export default function Book({ book }) {
-
   const colors = {
     history: "green",
     writing: "yellow",
     prophecy: "purple",
     paulsLetter: "blue",
-    generalLetter: "red"
+    generalLetter: "red",
   };
 
   return (
-    <div className={`p-2 bg-${colors[book.category]}-300 hover:bg-${colors[book.category]}-400 rounded-md shadow-md w-11 md:w-20 lg:w-24 xl:w-28 2xl:w-32`}>
-      <div className="text-gray-500 text-center hidden xl:block lg:text-xs 2xl:text-base">{book.name}</div>
-      <div className="text-sm md:text-2xl lg:text-3xl xl:text-5xl font-bold text-black text-center md:mb-2">{book.symbol}</div>
-      <div className="text-xs font-medium text-gray-500 text-center hidden xl:block">{book.author}</div>
-      <div className="text-xs font-medium text-gray-500 text-center hidden xl:block">{book.year}</div>
+    <div
+      className={`
+        w-11 
+        md:w-20 
+        lg:w-24 
+        xl:w-28 
+        2xl:w-32
+        p-2 
+        bg-${colors[book.category]}-300 
+        hover:bg-${colors[book.category]}-400 
+        rounded-md 
+        shadow-md 
+      `}
+    >
+      <div className="text-gray-500 text-center hidden xl:block xl:text-xs 2xl:text-sm">
+        {book.name}
+      </div>
+      <div className="text-sm md:text-2xl lg:text-3xl xl:text-5xl font-bold text-black text-center md:mb-2">
+        {book.symbol}
+      </div>
+      <div className="text-xs font-medium text-gray-500 text-center hidden xl:block">
+        {book.author}
+      </div>
+      <div className="text-xs font-medium text-gray-500 text-center hidden xl:block">
+        {book.year}
+      </div>
     </div>
   );
 }
